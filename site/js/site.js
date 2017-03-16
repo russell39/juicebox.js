d1142ee5d7bac4821831ad26c1a220d781bfdd5c
Muhammad S Shamim <sa501428@gmail.com>
2017-03-15 21:25:31 -0500

    Updated select combobox #35
    to use jquery autocomplete combobox widget. code adapted from
    http://jsfiddle.net/sschauhan/7qwaw0v1/
    
    Search within combobox needed for next step (i.e. large number of
    entries in combobox)

--- site/js/site.js
+++ site/js/site.js
@@ -1,28 +1,40 @@
 /**
  * Created by Jim Robinson on 3/4/17.
  */
-var hic = (function (hic) {
+ var hic = (function (hic) {
+ 	hic.initSite = function () {
 
-    hic.initSite = function () {
-        var site =
-        {
-            receiveEvent: function (event) {
-                if (event.type === "DataLoad") {
-                    var config = event.config,
-                        selector = '#dataset_selector option[value="' + config.url + '"]',
-                        $option = $(selector);
+ 		var browser = hic.createBrowser($('#app-container'), {});
 
-                    if ($option) $option.prop('selected', true);
+ 		$(function () {
+ 			$("#dataset_selector").combobox({
+ 				change: function () {
+ 					browser.loadHicFile({ url: this.value });
+ 				}
+ 			});
 
-                }
-            }
-        }
+ 			$("#toggle").click(function () {
+ 				$("#dataset_selector").toggle();
+ 			});
+ 		});
 
-        hic.GlobalEventBus.subscribe("DataLoad", site);
-    }
+ 		var site =
+ 		{
+ 			receiveEvent: function (event) {
+ 				if (event.type === "DataLoad") {
+ 					var config = event.config,
+ 					selector = '#dataset_selector option[value="' + config.url + '"]',
+ 					$option = $(selector);
 
+ 					if ($option) $option.prop('selected', true);
+ 				}
+ 			}
+ 		}
 
-    return hic;
+ 		hic.GlobalEventBus.subscribe("DataLoad", site);
+ 	}
 
-})
-(hic || {});
+ 	return hic;
+
+ })
+ (hic || {});
